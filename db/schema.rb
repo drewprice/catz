8e12fd0fac939a73fecd1aeadae589f261b0f277
Drew Price <drewprice@users.noreply.github.com>
2015-10-19 13:02:18 -0400

    Create postgresql db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151019040408) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cats", force: :cascade do |t|
     t.string   "giphy_id"
     t.datetime "created_at", null: false
