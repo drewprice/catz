295c7f54a6de795bd55c5281bb277736badeff22
Drew Price <drewprice@users.noreply.github.com>
2015-10-23 12:02:34 -0400

    Add fixed footer

--- app/assets/javascripts/cat.js
+++ app/assets/javascripts/cat.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
-  if ($('.alert')) {
-    window.setTimeout(function(){
-      $('.alert').slideUp(function(){
-        $(this).remove();
-      });
-    }, 4000);
-  }
+  window.setTimeout(function(){
+    hideAlert();
+  }, 4000);
+
+  $(window).keydown(function(e){
+    if (e.keyCode == 39) clickMoar();
+    if (e.keyCode == 32) clickLike();
+    if (e.keyCode == 76) clickLike();
+  })
 });
+
+
+function clickMoar(){
+  $('#moar').click()
+}
+
+function clickLike() {
+  $('#likable').click()
+}
+
+function hideAlert() {
+  $('.alert').slideUp(function(){
+    $(this).remove();
+  });
+}
